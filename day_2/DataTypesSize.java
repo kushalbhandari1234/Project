6dc9b11fe1d6d4255652496ff60ab440fbfca8bc
kushalbhandari1234 <kushalbhandari9998@gmail.com>
2025-11-03 08:43:11 +0545

    homework

---
+++ day_2/DataTypesSize.java
@@ -0,0 +1,40 @@
+package day_2;
+
+public class DataTypesSize{
+public static void main (string [] args)
+{
+
+int number=1234567890;
+
+String stringvariable="hello";
+Char charvariable='h';
+Short shortvariable=12345;
+Long longvariable=12345678900000L;
+Byte bytevariable=(byte)3444;
+//floating point 
+float temperature=36.667f;
+double gravity=9.80665;
+
+//integers 
+//int population=8_000_000_000;//Error without L!
+
+long world pop =8_000_000_000L;
+//correct
+
+//bases
+int binary=0b1010;  //10
+int octal=012;   //10
+int hexa=oxA;
+
+//other types
+byte small=(byte)127;
+char grade='A'
+boolean is Valid =true;
+string message ="literals demo";
+
+
+
+}
+
+
+}
